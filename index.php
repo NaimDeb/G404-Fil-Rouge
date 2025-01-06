7f5079a8fc3f1877fcba2bd7d531cb9e5b7559ca
naimdeb <naimdebard02@gmail.com>
2025-01-06 16:57:31 +0100

    index start

--- index.php
+++ index.php
@@ -6,6 +6,28 @@ require_once "./public/components/htmlstart.php";
 require_once "./public/components/header.php";
 ?>
 
+<main>
+
+<!-- Hero -->
+<section class="bg-[url(../src/images/heromobile.png)] h-[35vh] relative" alt="Image de couverture du site">
+
+<div class="w-fit h-fit p-8 flex flex-col gap-16">
+    <h1 class="text-2xl font-bold text-neutral-off-white ">Revendez vos livres, partagez vos histoire !</h1>
+
+    <button class="bg-primary-green rounded-md w-[75%] text-neutral-off-white text-xl py-2 px-4 text-nowrap">
+        <a href="./public/pages/inscription.php">
+            Vendez maintenant !
+        </a>
+    </button>
+
+</div>
+
+
+
+</section>
+
+
+</main>
 
 
 <?php 
