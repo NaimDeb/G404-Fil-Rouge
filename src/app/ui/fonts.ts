e122c4f787a9d81ad2937cfd1cb33e3a22e0152d
naimdeb <naimdebard02@gmail.com>
2025-03-28 16:32:04 +0100

    added header and footer

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Open_Sans, Merriweather } from 'next/font/google';
+
+export const merriweather = Merriweather({ subsets: ['latin'], weight: '400' });
+export const opensans = Open_Sans({ subsets: ['latin'], weight: '400' });
---
+++ src/app/ui/header/header.tsx
@@ -0,0 +1,155 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="bg-primary-green w-full h-[11vh] md:h-fit flex flex-col py-4 px-4 md:px-8">
+      <div className="md:h-full flex flex-col md:flex-row md:items-center md:justify-between">
+        {/* Première ligne  */}
+        <div className="h-full flex flex-row items-center justify-between max-md:w-full">
+          {/* Menu Hamburger  */}
+          <a
+            href="#"
+            className="md:hidden text-neutral-off-white text-2xl px-2"
+          >
+            <i className="fas fa-bars"></i>
+          </a>
+
+          {/* Logo  */}
+          <a href="./home.php" className="w-[80%] md:order-1 mx-auto md:px-2">
+            <Image
+              src="./assets/images/Logo.svg"
+              alt="Logo Bookmarket"
+              className="object-scale-down max-h-12 mx-auto sm:mx-0"
+            />
+          </a>
+
+          {/* User sm et moins  */}
+          {user ? (
+            <a
+              href="./profile.php"
+              className="flex gap-2 text-neutral-off-white text-2xl px-2 sm:order-3 md:hidden cursor-pointer items-center"
+            >
+              <span className="text-sm text-nowrap self-end ml-2">
+                {user.getUsername()}
+              </span>
+              <Image
+                src={`./assets/images/users/${user.getImage().getImgPath()}`}
+                alt="User Image"
+                className="rounded-full h-8 w-8"
+              />
+            </a>
+          ) : (
+            <a
+              href="./login.php"
+              className="sm:inline-flex text-neutral-off-white text-2xl px-2 sm:order-3 md:hidden cursor-pointer items-center"
+            >
+              <i className="fas fa-user"></i>
+            </a>
+          )}
+        </div>
+
+        {/* Searchbar */}
+        <div
+          id="searchbar"
+          className="bg-neutral-off-white py-2 px-4 w-full rounded-sm flex items-center justify-between gap-2 md:order-2 md:w-[3/5]"
+        >
+          {/* Dropdown et séparateur */}
+          <div className="flex items-center basis-1/5">
+            <label htmlFor="search-choice" className="sr-only">
+              Type de recherche
+            </label>
+            <select
+              name="searchChoice"
+              id="search-choice"
+              className="bg-neutral-off-white truncate text-sm font-bold w-full"
+              aria-label="Choisissez le type de recherche"
+            >
+              <option value="all">Tous</option>
+              <option value="book">Livres</option>
+              <option value="author">Auteurs</option>
+              <option value="user">Utilisateurs</option>
+            </select>
+            {/* Séparateur */}
+            <div
+              className="w-[1px] h-4 mx-2 bg-neutral-off-black"
+              aria-hidden="true"
+            ></div>
+          </div>
+
+          {/* Champ de recherche */}
+          <label htmlFor="search-input" className="sr-only">
+            Rechercher un livre
+          </label>
+          <input
+            type="text"
+            onKeyUp={showResult(this.value)}
+            placeholder="Vous cherchez un livre ? Recherchez ici..."
+            className="text-neutral-off-black focus:outline-none basis-3/5 w-full truncate"
+            aria-label="Rechercher un livre ou un utilisateur"
+            aria-live="polite"
+          />
+
+          {/* Icône de recherche */}
+          <i
+            className="fas fa-magnifying-glass text-xl basis-auto"
+            aria-label="Lancer la recherche"
+          ></i>
+        </div>
+
+        {/* User (md+) */}
+        {user ? (
+          <a
+            href="./profile.php"
+            className="md:inline-flex text-neutral-off-white text-2xl px-2 sm:order-3 hidden items-center"
+          >
+            <span className="text-sm text-nowrap self-end ml-2">
+              {user.username}
+            </span>
+            <Image
+              src="./assets/images/users/<?php echo $user->getImage()->getImgPath() ?>"
+              alt="User Image"
+              className="rounded-full h-8 w-8"
+            />
+          </a>
+        ) : (
+          <a
+            href="./login.php"
+            className="md:inline-flex text-neutral-off-white text-2xl px-2 sm:order-3 hidden items-center"
+          >
+            <span className="text-sm text-nowrap self-end mr-2">
+              Se connecter
+            </span>
+            <i className="fas fa-user"></i>
+          </a>
+        )}
+      </div>
+
+      {/* Deuxième ligne (Navigation supplémentaire) */}
+      <nav className="hidden md:flex w-full justify-between items-center mt-4 font-merriweather">
+        {/* Liens principaux */}
+        <div className="flex space-x-8 text-white font-bold basis-2/3 justify-between text-nowrap">
+          <a href="./new.php" className="hover:underline">
+            Nouveautés
+          </a>
+          <a href="./classics.php" className="hover:underline">
+            Classiques incontournables
+          </a>
+          <a href="./cheap.php" className="hover:underline">
+            Petits prix
+          </a>
+          <a href="./youth.php" className="hover:underline">
+            Jeunesse
+          </a>
+        </div>
+
+        {/* Bouton Vendre un livre */}
+        <a
+          href="./sell.php"
+          className="bg-transparent border-2 border-white text-white px-6 py-2 rounded-full hover:bg-white hover:text-primary-green transition"
+        >
+          Vendre un livre
+        </a>
+      </nav>
+    </header>
+  );
+}
